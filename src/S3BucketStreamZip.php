4aa558a479ca0b495a7c172166755f3e0c696dcb
Linus Metzler <linus.metzler@gmail.com>
2016-09-06 18:35:34 +0000

    Applied fixes from StyleCI

--- src/S3BucketStreamZip.php
+++ src/S3BucketStreamZip.php
@@ -17,42 +17,42 @@ use ZipStream;
 
 class S3BucketStreamZip
 {
-/**
-* @var array
-*
-* http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGET.html
-*
-* {
-*   key: access key,
-*   secret: access secret
-*   bucket: bucket name
-*   region: bucket region
-*   prefix: prefix
-* }
-*/
+    /**
+ * @var array
+ *
+ * http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGET.html
+ *
+ * {
+ *   key: access key,
+ *   secret: access secret
+ *   bucket: bucket name
+ *   region: bucket region
+ *   prefix: prefix
+ * }
+ */
 private $params = [];
 
 /**
-* @var object
-*/
+ * @var object
+ */
 private $s3Client;
 
 /**
-* Create a new ZipStream object.
-*
-* @param array $params   - AWS key, secret, region, and list object parameters
-*/
+ * Create a new ZipStream object.
+ *
+ * @param array $params - AWS key, secret, region, and list object parameters
+ */
 public function __construct($params)
 {
-foreach (['key', 'secret', 'bucket', 'region'] as $key) {
-if (!isset($params[$key])) {
-throw new InvalidParameterException('$params parameter to constructor requires a `'.$key.'` attribute');
-}
-}
+    foreach (['key', 'secret', 'bucket', 'region'] as $key) {
+        if (!isset($params[$key])) {
+            throw new InvalidParameterException('$params parameter to constructor requires a `'.$key.'` attribute');
+        }
+    }
 
-$this->params = $params;
+    $this->params = $params;
 
-$this->s3Client = new S3Client([
+    $this->s3Client = new S3Client([
 'region'      => $this->params['region'],
 'version'     => 'latest',
 'credentials' => [
@@ -63,19 +63,19 @@ $this->s3Client = new S3Client([
 }
 
 /**
-* Stream a zip file to the client.
-*
-* @param string $filename  - Name for the file to be sent to the client
-* @param array  $params    - Optional parameters
-*  {
-*    expiration: '+10 minutes'
-*  }
-*/
+ * Stream a zip file to the client.
+ *
+ * @param string $filename - Name for the file to be sent to the client
+ * @param array  $params   - Optional parameters
+ *                         {
+ *                         expiration: '+10 minutes'
+ *                         }
+ */
 public function send($filename, $params = [])
 {
-// Set default values for the optional $params argument
+    // Set default values for the optional $params argument
 if (!isset($params['expiration'])) {
-$params['expiration'] = '+10 minutes';
+    $params['expiration'] = '+10 minutes';
 }
 
 // Initialize the ZipStream object and pass in the file name which
@@ -93,13 +93,13 @@ $result = $this->s3Client->getIterator('ListObjects',
 
 // We loop over each object from the ListObjects call.
 foreach ($result as $file) {
-// We need to use a command to get a request for the S3 object
+    // We need to use a command to get a request for the S3 object
 //  and then we can get the presigned URL.
 $command = $this->s3Client->getCommand('GetObject', [
 'Bucket' => $this->params['bucket'],
 'Key'    => $file['Key'],
 ]);
-$signedUrl = (string) $this->s3Client->createPresignedRequest($command, $params['expiration'])->getUri();
+    $signedUrl = (string) $this->s3Client->createPresignedRequest($command, $params['expiration'])->getUri();
 
 
 // Get the file name on S3 so we can save it to the zip file
@@ -114,14 +114,14 @@ $fileName = basename($file['Key']);
 //  pointer.
 // Closing the file pointer removes the file.
 $fp = tmpfile();
-$ch = curl_init($signedUrl);
-curl_setopt($ch, CURLOPT_TIMEOUT, 120);
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_exec($ch);
-curl_close($ch);
-$zip->addFileFromStream($fileName, $fp);
-fclose($fp);
+    $ch = curl_init($signedUrl);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch);
+    curl_close($ch);
+    $zip->addFileFromStream($fileName, $fp);
+    fclose($fp);
 }
 
 // Finalize the zip file.
